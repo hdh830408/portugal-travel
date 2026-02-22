5d8867c5d00e3a6a5bb9f16a37590d7346818c00
hdh830408 <hdh830408@gmail.com>
2026-02-22 20:18:05 +0900

    Update source code

--- sw.js
+++ sw.js
@@ -1,48 +1,42 @@
 const CACHE_NAME = 'portugal-travel-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/portugal_data.js',
-  '/portugal_guides.js',
-  '/coords_data.js'
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './portugal_data.js',
+  './portugal_guides.js',
+  'https://cdn-icons-png.flaticon.com/512/197/197463.png'
 ];
 
-// Install event
-self.addEventListener('install', event => {
+// 1. 설치 (Install): 캐시 저장
+self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Install');
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
   );
 });
 
-// Fetch event
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
+// 2. 활성화 (Activate): 이전 캐시 정리
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
         }
-        return fetch(event.request);
-      }
-    )
+      }));
+    })
   );
 });
 
-// Activate event
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(cacheName => {
-          return cacheName !== CACHE_NAME;
-        }).map(cacheName => {
-          return caches.delete(cacheName);
-        })
-      );
+// 3. 요청 (Fetch): 오프라인이면 캐시에서 꺼내줌
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
